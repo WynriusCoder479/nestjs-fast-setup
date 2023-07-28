b8a4dfedb800d6252db7b52d43df47ccf439a725
WynriusCoder479 <wynrius12345678@gmail.com>
2023-07-28 19:34:12 +0700

    initial app

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+	singleQuote: true,
+	trailingComma: "all",
+	arrowParens: "always",
+	bracketSameLine: true,
+	bracketSpacing: true,
+	editorconfig: true,
+	endOfLine: "lf",
+	semi: true,
+	quoteProps: "as-needed",
+	useTabs: true,
+	tabWidth: 2,
+};
